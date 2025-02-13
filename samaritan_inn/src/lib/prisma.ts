2571e9269cd40638527eabd7f2dc91afdc235031
Lauren <145627044+Laursaurus@users.noreply.github.com>
2025-02-12 21:12:45 -0600

    updates prisma.ts file

--- samaritan_inn/src/lib/prisma.ts
+++ samaritan_inn/src/lib/prisma.ts
@@ -1,4 +1,14 @@
-// src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+/// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
